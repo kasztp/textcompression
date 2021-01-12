90ab352b3db5173b0346242d365956673658cb6e
kaszt <kasztp@gmail.com>
2021-01-13 00:06:50 +0100

    Improved compression efficiency by using Base98 instead of hex for dictionary indexes. Added dictionary_length to compressed response.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# Name:         Text Compression Service v0.5
+# Name:         Text Compression Service v0.6
 # Maintainer:   Peter Kaszt (peter@talentstack.eu)
-# Date:         2020.10.08.
+# Date:         2021.01.12.
 #
 # Usage:    Run the app as standalone if you have Flask installed.
 #           Modify host parameter for flask according to your needs.
@@ -15,14 +15,23 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "nobody-gonna-guess-it"
 
 
+def base_x(number):
+    b_x = str()
+    digits = '0123456789AÁBCDEÉFGHIÍJKLMNOÓÖŐPQRSTUÚÜŰVWXYZaábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz'
+    while number != 0:
+        number, i = divmod(number, len(digits))
+        b_x = digits[i] + b_x
+    return b_x
+
+
 def pack(payload, input_type):
     if input_type == "url":
         response = requests.request("GET", payload, allow_redirects=True)
-        status_code = response.status_code
+        status_code = response.status_code  # to be used for tests.
         data = response.text
     else:
-        data =payload
-        #print(data)
+        data = payload
+        # print(data)
     original_length = len(data)
     print(f"Downloaded text size: {original_length}")
     text = list()
@@ -30,21 +39,22 @@ def pack(payload, input_type):
     for line in data.splitlines(keepends=True):
         text.append(line.split())
     print(f"Text lines: {len(text)}")
-    #print(text)
+    # print(text)
     # Generate word dictionaries for compression & decompression
     unique_words = list(set(itertools.chain(*text)))
     for word in unique_words:
         if len(word) <= 2:
             unique_words.remove(word)
     unique_words.sort(key=len)
-    #print(unique_words)
+    # print(unique_words)
     extract_dict = dict()
     compress_dict = dict()
     for index, word in enumerate(unique_words):
-        #print(f"Index: {index}  -  Word: {word}")
+        # print(f"Index: {index}  -  Word: {word}")
         extract_dict[str(hex(index))[2:]] = word
-        compress_dict[word] = str(hex(index)[2:])
-    print(f"Compress dictionary size: {len(compress_dict)}")
+        compress_dict[word] = base_x(index)
+    dictionary_length = sys.getsizeof(compress_dict)
+    print(f"Compress dictionary size: {dictionary_length}")
     # Compress text
     for row, line in enumerate(text):
         for column, word in enumerate(line):
@@ -63,6 +73,7 @@ def pack(payload, input_type):
         "original_length": original_length,
         "compressed_length": compressed_length,
         "dictionary": extract_dict,
+        "dictionary_length": dictionary_length,
         "text": compressed_text
     }
     return result
@@ -71,7 +82,7 @@ def pack(payload, input_type):
 def extract(data):
     # Extract original text
     extract_dict = data["payload"]["dictionary"]
-    #print(type(extract_dict))
+    # print(type(extract_dict))
     compressed_text = data["payload"]["text"]
     print(f"Compressed text: {compressed_text}")
     keys = list()
@@ -79,7 +90,7 @@ def extract(data):
         keys.append(int(key))
     for key in sorted(keys, reverse=True):
         value = extract_dict.get(str(key))
-        #print(f"{key} : {value}")
+        # print(f"{key} : {value}")
         compressed_text = compressed_text.replace(str(key), value)
     print(f"Decompressed text: {compressed_text}")
     # Prepare response
@@ -129,9 +140,10 @@ def compress():
     data = request.get_json(silent=True)
     request_size = request.content_length
     request_json_size = sys.getsizeof(request.get_json(silent=True))
+
     print(f"Incoming request size: {request_size}")
     print(f"Incoming JSON size: {request_json_size}")
-    #print(f"Incoming request:\n{data}")
+    # print(f"Incoming request:\n{data}")
     mode = data["mode"]
     print(f"\nRequest type: {mode}")
     if mode == "compress":
@@ -143,10 +155,10 @@ def compress():
         response = extract(data)
     else:
         response = "Wrong mode! Only compress/extract are supported."
-    #print(f"Response:\n{response}")
+    # print(f"Response:\n{response}")
     print(f"Response size: {sys.getsizeof(response)}")
     return response
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
